50492f3d89c4ec43e6152830cd80dd7c0847a2ba
DocenteITApulia <docentedev2022mf@studenti.apuliadigitalmaker.it>
2021-09-17 13:46:29 +0200

    first draft

---
+++ src/main/java/it/apulia/esercitazione1ter/Esercitazione1terApplication.java
@@ -0,0 +1,13 @@
+package it.apulia.esercitazione1ter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercitazione1terApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercitazione1terApplication.class, args);
+	}
+
+}
---
+++ src/main/java/it/apulia/esercitazione1ter/LoginDTO.java
@@ -0,0 +1,9 @@
+package it.apulia.esercitazione1ter;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    String email;
+    String password;
+}
